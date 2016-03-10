6e1461ba5717f46fdd0c815e5fd422e832f7802b
ayoub <ayoub.belghar@gail.com>
2016-03-10 19:15:36 +0000

    load more

--- microscope/server/publications.js
+++ microscope/server/publications.js
@@ -7,6 +7,11 @@ Meteor.publish("posts", function(options){
 	return Posts.find({}, options);
 });	
 
+Meteor.publish('singlePost', function(id){
+	check(id, String);
+	return Posts.find(id);
+});
+
 Meteor.publish("comments", function(postId){
 	check(postId, String);
 	return Comments.find({postId: postId});
@@ -15,3 +20,4 @@ Meteor.publish("comments", function(postId){
 Meteor.publish("notifications", function(){
 	return Notifications.find({userId: this.userId, read: false});
 });
+
